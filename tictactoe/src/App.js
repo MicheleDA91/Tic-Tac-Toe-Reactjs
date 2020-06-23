103d1ac79559e93ae0a879b61b1ab1688827746a
MicheleDA91 <Michele.dellarciprete@hotmail.it>
2020-06-23 12:04:19 +0200

    tictactoegame

---
+++ tictactoe/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Game from './components/Game';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+       </header>
+       <div>
+         <Game />
+       </div>
+    </div>
+  );
+}
+
+export default App;
